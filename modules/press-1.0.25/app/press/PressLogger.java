bacd49a570fd08d472e5120c07fd4cf4e93a6d48
Sébastien Cesbron <scesbron@gmail.com>
2012-05-30 22:55:45 +0200

    remove modules directory

--- modules/press-1.0.25/app/press/PressLogger.java
+++
@@ -1,9 +0,0 @@
-package press;
-
-import play.Logger;
-
-public class PressLogger {
-    public static void trace(String message, Object... args) {
-        Logger.trace("Press: " + message, args);
-    }
-}
